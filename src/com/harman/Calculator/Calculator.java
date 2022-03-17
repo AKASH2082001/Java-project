501114fa7e2ac5e7eb223a5b15243fa3495cdd22
AKASH2082001 <akashbme208@gmail.com>
2022-03-17 14:02:15 -0700

    Calculation

---
+++ src/com/harman/Calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.harman.Calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        int c = x + y;
+        System.out.println(c);
+    }
+
+}
